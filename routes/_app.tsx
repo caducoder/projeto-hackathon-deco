69bb064310cbc5cdbd3470167151f65ab4a2f366
caducoder <cadudrop@hotmail.com>
2023-11-04 00:23:29 -0300

    feat: projeto hackathon deco

---
+++ routes/_app.tsx
@@ -0,0 +1,32 @@
+import { AppProps } from "$fresh/server.ts";
+import GlobalTags from "$store/components/GlobalTags.tsx";
+import Theme from "$store/sections/Theme/Theme.tsx";
+
+const sw = () =>
+  addEventListener("load", () =>
+    navigator && navigator.serviceWorker &&
+    navigator.serviceWorker.register("/sw.js"));
+
+function App(props: AppProps) {
+  return (
+    <>
+      {/* Include default fonts and css vars */}
+      <Theme />
+
+      {/* Include Icons and manifest */}
+      <GlobalTags />
+
+      {/* Rest of Preact tree */}
+      <props.Component />
+
+      {/* Include service worker */}
+      <script
+        type="module"
+        dangerouslySetInnerHTML={{ __html: `(${sw})();` }}
+      />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    </>
+  );
+}
+
+export default App;
